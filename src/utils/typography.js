92149078d5a5f16889c4228277365bb63ba1d885
Raúl Ojeda <rauloojs@gmail.com>
2018-11-15 16:52:53 -0600

    feature(utils typography): Remove third party theme and set up custom conf for typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,26 +1,28 @@
 import Typography from "typography"
-import fairyGatesTheme from 'typography-theme-fairy-gates'
 
-fairyGatesTheme.headerWeight = '400';
-fairyGatesTheme.googleFonts = [
-  {
-    name: 'Work Sans',
-    styles: ['200', '400', '600'],
-  },
-  {
-    name: 'Lato',
-    styles: ['400'],
-  },
-  {
-    name: 'Cabin',
-    styles: ['600'],
-  },
-  {
-    name: 'Quattrocento Sans',
-    styles: ['400', '400i', '700'],
-  },
-]
-
-const typography = new Typography(fairyGatesTheme)
+const typography = new Typography({
+  baseFontSize: '20px',
+  baseLineHeight: 1.45,
+  googleFonts: [
+    {
+      name: 'Lato',
+      styles: ['400'],
+    },
+    {
+      name: 'Cabin',
+      styles: ['400', '600', '700'],
+    },
+  ],
+  headerWeight: '400',
+  headerFontFamily: ['Lato', 'sans-serif'],
+  bodyFontFamily: ['Cabin', 'sans-serif'],
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
+    a: {
+      textDecoration: 'none',
+    }
+  })
+})
 
 export default typography
