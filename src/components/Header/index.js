316dcce134d70e2a83db78aba2a158e32a515640
Raúl Ojeda <rauloojs@gmail.com>
2018-11-15 22:26:00 -0600

    feature(components Header): Make Header a stateful component to handle scroll changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,37 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import { FaCode } from 'react-icons/fa';
+import HeaderWrapper from './HeaderWrapper';
 import LinkWrapper from './LinkWrapper';
 import Title from './Title';
 import Subtitle from './Subtitle';
 import TitleWrapper from './TitleWrapper';
 import Navbar from '../Navbar';
 
-const Header = ({ siteTitle }) => (
-  <div>
-    <div className="f-row">
-      <LinkWrapper className="col-xs-12">
-        <Link
-          to="/"
-          className="f-row center-xs middle-xs"
-        >
-          <FaCode style={{
-            'marginRight': '0.5em',
-            'fontSize': '1.3em',
-          }}/>
-          <TitleWrapper>
-            <Title>
-              {siteTitle}
-            </Title>
-            <Subtitle>
-              Full Stack Web Developer
-            </Subtitle>
-          </TitleWrapper>
-        </Link>
-      </LinkWrapper>
-      <Navbar />
-    </div>
-  </div>
-)
+let scrollListener = null;
+
+class Header extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      headerClass: ""
+    };
+  }
+
+  componentDidMount () {
+    scrollListener = (event) => {
+      this.setState({
+        headerClass: (window.scrollY > 0) ? "sticky" : ""
+      })
+    };
+    window.addEventListener("scroll", scrollListener)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener("scroll", scrollListener);
+  }
+
+  render() {
+    return (
+      <HeaderWrapper className={this.state.headerClass}>
+        <div className="f-row">
+          <LinkWrapper className="col-xs-12">
+            <Link
+              to="/"
+              className="f-row center-xs middle-xs"
+            >
+              <FaCode style={{
+                'marginRight': '0.5em',
+                'fontSize': '1.3em',
+              }}/>
+              <TitleWrapper>
+                <Title>
+                  {this.props.siteTitle}
+                </Title>
+                <Subtitle>
+                  Full Stack Web Developer
+                </Subtitle>
+              </TitleWrapper>
+            </Link>
+          </LinkWrapper>
+          <Navbar />
+        </div>
+      </HeaderWrapper>
+    );
+  }
+}
 
 export default Header
