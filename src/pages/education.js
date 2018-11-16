43072d62fbb36f8ca82a298b6f27c43bc6937af6
Raúl Ojeda <rauloojs@gmail.com>
2018-11-15 21:04:11 -0600

    feature(pages education): Add skeleton for Education page

---
+++ src/pages/education.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const EducationPage = ({ data }) => (
+  <Layout>
+    <h1>Education</h1>
+  </Layout>
+)
+
+export default EducationPage
