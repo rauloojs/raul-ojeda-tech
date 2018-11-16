04045c93e5a6c94b4f56c43ae7d6eb28c15494ab
Raúl Ojeda <rauloojs@gmail.com>
2018-11-15 21:05:06 -0600

    feature(pages skills): Add skeleton for Skills page

---
+++ src/pages/skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const SkillsPage = ({ data }) => (
+  <Layout>
+    <h1>Skills</h1>
+  </Layout>
+)
+
+export default SkillsPage
