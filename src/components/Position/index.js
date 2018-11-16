a4836ef03c3444385edaef43c79d102e0d027ecb
Raúl Ojeda <rauloojs@gmail.com>
2018-11-15 19:25:13 -0600

    feature(components Position): Add list of tags

--- src/components/Position/index.js
+++ src/components/Position/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tags from '../Tags';
 import PositionWrapper from './PositionWrapper';
 import Title from './Title';
 import Company from './Company';
 import Dates from './Dates';
 import Description from './Description';
 
-const Position = ({ title, company, dates, description }) => {
+const Position = ({ title, company, dates, description, tags }) => {
   return (
     <PositionWrapper>
       <Title>{title}</Title>
       <Company>{company}</Company>
       <Dates>{dates}</Dates>
       <Description>{description}</Description>
+      <Tags tags={tags} />
     </PositionWrapper>
   );
 };
