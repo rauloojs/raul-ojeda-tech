48ae44c39f76eddcd53087e71eb76cec431e972f
Raúl Ojeda <rauloojs@gmail.com>
2018-11-26 21:26:27 -0600

    feature(components ListContainer): Fix responsive behaviour

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -5,11 +5,11 @@ import ContentWrapper from './ContentWrapper';
 
 const ListContainer = ({ list, content, selectedItem }) => {
   return (
-    <div className="w-100 flex items-start">
-      <ListWrapper className={`${selectedItem ? 'db-ns dn w-30-ns' : 'w-100'}`}>
+    <div className="w-100 flex self-stretch">
+      <ListWrapper className={`${selectedItem ? 'db-ns dn w-40-m w-30' : 'w-100'}`}>
         {list}
       </ListWrapper>
-      <ContentWrapper className={`${selectedItem ? 'w-100 w-70-ns' : ' dn w-0'}`}>
+      <ContentWrapper className={`${selectedItem ? 'w-100 w-60-m w-70' : ' dn w-0'}`}>
         {content}
       </ContentWrapper>
     </div>
