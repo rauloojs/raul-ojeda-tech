86e8ff2dffe31cbf806dadaa1640df4f717aa5df
Raúl Ojeda <rauloojs@gmail.com>
2018-11-26 19:25:09 -0600

    chore(storybook config): Add project styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,10 @@
 import { configure } from '@storybook/react';
 import typography from '../src/utils/typography';
 
+import '../src/css/main.css'
+import 'tachyons/css/tachyons.min.css'
+import 'flexboxgrid/dist/flexboxgrid.min.css'
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
