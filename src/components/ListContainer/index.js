3b98b4a99dcd1826571706fb2dc5fed81393f255
Raúl Ojeda <rauloojs@gmail.com>
2018-11-26 19:37:07 -0600

    chore(components ListContainers): Add propTypes

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -6,10 +6,10 @@ import ContentWrapper from './ContentWrapper';
 const ListContainer = ({ list, content, selectedItem }) => {
   return (
     <div className="w-100 flex items-start">
-      <ListWrapper className={`${selectedItem ? 'dn db-ns w-30-ns' : 'w-100'}`}>
+      <ListWrapper className={`${selectedItem ? 'db-ns dn w-30-ns' : 'w-100'}`}>
         {list}
       </ListWrapper>
-      <ContentWrapper className={`${selectedItem ? 'w-100 w-70-ns' : 'w-0'}`}>
+      <ContentWrapper className={`${selectedItem ? 'w-100 w-70-ns' : ' dn w-0'}`}>
         {content}
       </ContentWrapper>
     </div>
@@ -17,7 +17,9 @@ const ListContainer = ({ list, content, selectedItem }) => {
 };
 
 ListContainer.propTypes = {
-  
+  list: PropTypes.node.isRequired,
+  content: PropTypes.node.isRequired,
+  selectedItem: PropTypes.any
 };
 
 export default ListContainer;
