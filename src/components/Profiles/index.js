b5e6b60b656a176bb543727a8ef208a68f2012ca
Raúl Ojeda <rauloojs@gmail.com>
2018-11-28 15:22:52 -0600

    feature(components Profiles): Pass in className to wrapper

--- src/components/Profiles/index.js
+++ src/components/Profiles/index.js
@@ -3,32 +3,27 @@ import Card from '../Card';
 import { FaClock, FaLinkedinIn, FaGithub, FaFile } from 'react-icons/fa';
 
 import cv from '../../files/CV.pdf';
-import ProfilesWrapper from './ProfilesWrapper';
 
-const Profiles = () => {
+const Profiles = ({ className }) => {
   return (
-    <ProfilesWrapper className="f-row center-xs">
+    <div className={className}>
       <Card
-        className="col-xs-6"
         icon={<FaLinkedinIn />}
         link="https://www.linkedin.com/in/rauloojs/"
         title="My LinkedIn profile" />
       <Card
-        className="col-xs-6"
         icon={<FaGithub />}
         link="https://github.com/rauloojs"
         title="My GitHub profile" />
       <Card
-        className="col-xs-6"
         icon={<FaClock />}
         link="https://wakatime.com/@rauloojs"
         title="My Wakatime profile" />
       <Card
-        className="col-xs-6"
         icon={<FaFile />}
         link={cv}
         title="Download my resume" />
-    </ProfilesWrapper>
+    </div>
   );
 };
 
