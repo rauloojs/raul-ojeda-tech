2f57733e001a9e34d9af24c486f924d3dd462c05
Raúl Ojeda <rauloojs@gmail.com>
2018-11-27 19:55:45 -0600

    feature(components SiteTitle): Move site title element to its own component. Use tachyons classes

---
+++ src/components/SiteTitle/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from '@reach/router';
+import { FaCode } from 'react-icons/fa';
+
+const SiteTitle = ({ small }) => {
+  return (
+    <Link
+      to="/"
+      className="flex items-center black-80"
+    >
+      <FaCode className={`${small ? 'f3 mr1' : 'f1 mr3'}`} />
+      <div className="flex flex-column">
+        <h2 className={`ma0 tracked-tight ${small ? 'f3' : 'f1'}`}>
+          Raúl <span className="brand-color">Ojeda</span>
+        </h2>
+        <h3 className={`f5 pl1 ma0 mt1 black-50 ${small && 'dn'}`}>
+          Full Stack Web Developer
+        </h3>
+      </div>
+    </Link>
+  );
+};
+
+SiteTitle.propTypes = {
+  small: PropTypes.bool
+};
+
+export default SiteTitle;
