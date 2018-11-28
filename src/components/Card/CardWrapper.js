f82b602d543636166ba9e8ea30dc60e70c8137b6
Raúl Ojeda <rauloojs@gmail.com>
2018-11-28 15:09:52 -0600

    feature(components Card): Change CardWrapper styles

--- src/components/Card/CardWrapper.js
+++ src/components/Card/CardWrapper.js
@@ -1,22 +1,20 @@
 import styled from 'styled-components';
 
 const CardWrapper = styled.a`
-  background-image: none;
+  background-color: white;
   width: auto;
-  min-width: 150px;
-  min-height: 150px;
-  font-size: 2.5em;
+  font-size: 1.5rem;
+  padding: 1rem;
   color: #676767;
   cursor: pointer;
-  transition: all 0.8s;
+  transition: all 0.4s;
 
-  &:hover {
-    color: inherit;
-    background-color: #ebebeb;
-
-    & > svg {
-      transition: all 0.8s;
-      transform: scale(1.2);
+  @media only screen and (min-width: 960px) {
+    &:hover {
+      color: inherit;
+      background-color: #ebebeb;
+      padding: 1.5rem;
+      font-size: 2rem;
     }
   }
 `;
