a86b6429ef033fa6b97e0644d4432148aefe90e9
Raúl Ojeda <rauloojs@gmail.com>
2018-11-27 21:26:44 -0600

    feature(components Header, Backdrop): Add Backdrop component for Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import throttle from 'lodash/throttle';
 import HeaderWrapper from './HeaderWrapper';
 import Navbar from '../Navbar';
 import SiteTitle from '../SiteTitle';
+import Backdrop from '../Backdrop';
 
 let scrollListener, resizeListener = null;
 
@@ -56,20 +57,25 @@ class Header extends Component {
     const small = this.state.scrolled || this.state.smallViewport;
 
     return (
-      <HeaderWrapper
-        smallViewport={this.state.smallViewport}
-        scrolled={this.state.scrolled}
-        hideNavbar={this.state.hideNavbar}
-        className={`w-100 flex items-start items-center-l fixed ${small ? ' pt0-l flex-column flex-row-l justify-between-l ph3 shadow-bottom justify-center': 'pt4 pb2 flex-column justify-between'}`}
-      >
-        <SiteTitle small={small} />
-        <FaBars
-          className="absolute dn-l right-1 pointer op-90 glow"
-          style={{ top: 16 }}
-          onClick={this.handleNavbarToggle}
-        />
-        <Navbar hide={this.state.smallViewport && this.state.hideNavbar} />
-      </HeaderWrapper>
+      <div>
+        {!this.state.hideNavbar && (
+          <Backdrop onClick={this.handleNavbarToggle} />
+        )}
+        <HeaderWrapper
+          smallViewport={this.state.smallViewport}
+          scrolled={this.state.scrolled}
+          hideNavbar={this.state.hideNavbar}
+          className={`w-100 flex items-start items-center-l fixed ${small ? ' pt0-l flex-column flex-row-l justify-between-l ph3 shadow-bottom justify-center': 'pt4 pb2 flex-column justify-between'}`}
+        >
+          <SiteTitle small={small} />
+          <FaBars
+            className="absolute dn-l right-1 pointer op-90 glow"
+            style={{ top: 16 }}
+            onClick={this.handleNavbarToggle}
+          />
+          <Navbar hide={this.state.smallViewport && this.state.hideNavbar} />
+        </HeaderWrapper>
+      </div>
     );
   }
 }
