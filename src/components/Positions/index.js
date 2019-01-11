e889e1198b34580b3959ff76a0429a48d2f9d3a7
Raúl Ojeda <rauloojs@gmail.com>
2019-01-11 01:16:06 -0600

    feature(components Positions): Remove ListContainer and use simple mapping to Position or PositionListItem

--- src/components/Positions/index.js
+++ src/components/Positions/index.js
@@ -1,41 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Position from '../Position';
-import ListContainer from '../ListContainer';
+import PositionListItem from '../PositionListItem';
 
-const Positions = ({ positions, selectedItem, onSelectItem }) => {
+const Positions = ({ positions, asList }) => {
   return (
-    <ListContainer
-      list={(
-        <div className={`ml3 h-100 ${selectedItem ? 'br3 paper-1 pv2' : 'flex flex-column items-center'}`}>
-          {positions.map(({ node }) => (
-            <Position
-              key={node.id}
-              item={node}
-              selectedItem={selectedItem}
-              onClick={onSelectItem}
-            />
-          ))}
-        </div>
-      )
-      }
-      content={(
-        selectedItem && (
-          <div className="pa4 br3 paper-1 mh2 w-auto w-90-l">
-            <h2>{selectedItem.frontmatter.title}</h2>
-            <h4 className="f6 black-60 mb1">{selectedItem.frontmatter.company}</h4>
-            <h4 className="f6 black-30 mb4">{selectedItem.frontmatter.dates}</h4>
-            <div dangerouslySetInnerHTML={{ __html: selectedItem.html }}></div>
-          </div>
-        )
-      )}
-      selectedItem={selectedItem}
-    />
+    <div className={`ml3 h-100 ${asList ? 'br3 paper-1 pv2' : 'flex flex-column items-center'}`}>
+      {positions.map(({ node }) => asList ? (
+        <PositionListItem
+          key={node.id}
+          position={node}
+        />
+      ) : (
+        <Position
+          key={node.id}
+          position={node}
+        />
+      ))}
+    </div>
   );
 };
 
 Positions.propTypes = {
   positions: PropTypes.array.isRequired,
+  asList: PropTypes.bool,
 };
 
 export default Positions;
