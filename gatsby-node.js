6077a88c9d3fc0fe78256b72c34ab6f5b32c2111
Raúl Ojeda <rauloojs@gmail.com>
2019-01-11 01:11:33 -0600

    feature(gatsby-node): Generate paths for markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return graphql(`
+    {
+      allMarkdownRemark(sort: {order: DESC, fields: [fileAbsolutePath]}) {
+        edges {
+          node {
+            id,
+            frontmatter {
+              path,
+              title,
+              company,
+              tags {
+                label,
+                class
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: path.resolve(`./src/templates/Position/index.js`),
+        context: {
+          positions: result.data.allMarkdownRemark.edges,
+          pathId: node.frontmatter.path,
+        },
+      });
+    });
+  })
+};
