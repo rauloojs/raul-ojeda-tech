6a6ed9c35f73cb05aeca148982e1f715044fbee5
Raúl Ojeda <rauloojs@gmail.com>
2019-01-11 15:04:20 -0600

    feature(pages education): Use education component to render section

--- src/pages/education.js
+++ src/pages/education.js
@@ -4,18 +4,12 @@ import Layout from '../components/Layout'
 import Courses from '../components/Courses';
 import Languages from '../components/Languages';
 import Container from '../components/Container';
-import Paper from '../components/Paper';
+import Education from '../components/Education';
 
 const EducationPage = ({ data }) => (
   <Layout>
     <Container>
-      <Paper>
-        <div className="flex flex-column">
-          <span>B.S. Computer Sciences Degree</span>
-          <span>Benemérita Universidad Autónoma de Puebla</span>
-          <span>August 2011 - December 2015</span>
-        </div>
-      </Paper>
+      <Education />
       <Languages />
       <Courses />
     </Container>
