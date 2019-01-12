dedb1db2ba2c72b384940667d944530c12ee1170
Raúl Ojeda <rauloojs@gmail.com>
2019-01-11 18:50:03 -0600

    chore(components Container): Adjust padding top

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({ children }) => {
   return (
-    <div className="flex flex-column items-center pa3">
+    <div className="flex flex-column items-center ph3 pb3">
       <div className="measure-wide">
         {children}
       </div>
