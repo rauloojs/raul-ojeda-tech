fab2571836aa2982c6a52dc04d42fbc2303edc98
Raúl Ojeda <rauloojs@gmail.com>
2019-01-11 18:51:03 -0600

    chore(components Couse): Adjust font size)

--- src/components/Course/CourseWrapper.js
+++ src/components/Course/CourseWrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const CourseWrapper = styled.div`
   display: flex;
   flex-flow: column;
-  font-size: 0.7rem;
+  font-size: 0.9rem;
   padding-bottom: 1rem;
 `;
 
