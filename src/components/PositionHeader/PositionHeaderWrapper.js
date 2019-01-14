43f42ca055ac07138a6d94f1c0e2bcc9f9d2081b
Raúl Ojeda <rauloojs@gmail.com>
2019-01-14 14:12:05 -0600

    feature(components PositionHeader): Add header component for Positions template

---
+++ src/components/PositionHeader/PositionHeaderWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const PositionHeaderWrapper = styled.div`
+  background: linear-gradient(to right, #cc2b5e, #753a88);
+  padding: 2rem;
+  color: white;
+  margin-bottom: 0.5rem;
+`;
+
+export default PositionHeaderWrapper;
---
+++ src/components/PositionHeader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PositionHeaderWrapper from './PositionHeaderWrapper';
+
+const PositionHeader = ({ title, company, dates }) => {
+  return (
+    <PositionHeaderWrapper className="paper-1 pa4 br3 mh2 w-auto w-90-l">
+      <h1>{title}</h1>
+      <h4 className="white-90 mb1">{company}</h4>
+      <h4 className="white-60 mb0">{dates}</h4>
+    </PositionHeaderWrapper>
+  );
+};
+
+export default PositionHeader;
