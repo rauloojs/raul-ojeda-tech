5d8ef942b4e28a207b252cd98ecfe351247fdca1
Raúl Ojeda <rauloojs@gmail.com>
2019-01-15 17:45:56 -0600

    feature(components PositionHeader): Add link to company site. Adjust padding

--- src/components/PositionHeader/index.js
+++ src/components/PositionHeader/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PositionHeaderWrapper from './PositionHeaderWrapper';
 
-const PositionHeader = ({ title, company, dates, gradient }) => {
+const PositionHeader = ({ title, company, companySite, dates, gradient }) => {
   return (
     <PositionHeaderWrapper
-      className="paper-1 pa4 br3 mh2 w-auto w-90-l"
+      className="paper-1 pa3 pa4-l br3 mh2 w-auto w-90-l"
       gradient={gradient}
     >
       <h1>{title}</h1>
-      <h4 className="white-90 mb1">{company}</h4>
+      <a href={companySite}><h3 className="white-90 mb1">{company}</h3></a>
       <h4 className="white-60 mb0">{dates}</h4>
     </PositionHeaderWrapper>
   );
