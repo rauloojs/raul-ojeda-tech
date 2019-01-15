6ee4090e4d4980213bde0f9b199ee32e55b6205b
Raúl Ojeda <rauloojs@gmail.com>
2019-01-15 17:49:20 -0600

    chore(pages 404): Adjust padding-top

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="w-100 tc pt-header ph5 lh-copy">
+    <div className="w-100 tc pt5 ph5 lh-copy">
       <h1 className="f1 f-headline-l code mb3 fw9 dib tracked-tight">404</h1>
       <h2 class="tc f1-l fw1">Sorry, the page you're looking for doesn't exist.</h2>
     </div>
