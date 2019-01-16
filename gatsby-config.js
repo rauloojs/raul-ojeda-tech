bf4e01098fb9efe0dc84d4b2c7cbea73f26c886e
Raúl Ojeda <rauloojs@gmail.com>
2019-01-15 19:54:51 -0600

    feature(package.json, gatsby-config): Add gatsby-plugin-manifest to generate manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,20 @@ module.exports = {
         ],
         whitelistPatterns: [/^bg-/],
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Raúl Ojeda's personal site",
+        short_name: "Raul Ojeda",
+        start_url: "/",
+        background_color: "#318ada",
+        theme_color: "#318ada",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
+      },
+    },
   ],
 }
